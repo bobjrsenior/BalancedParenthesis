02c561aba8569c8c78fdc0916e2f61aa00ffdae8
bobjrsenior <bobjrsenior@gmail.com>
2016-10-31 18:02:06 -0400

    Added solution

--- balancedParentheses.go
+++ balancedParentheses.go
@@ -2,6 +2,46 @@ package main
 
 import "fmt"
 
+type node struct {
+	Prev *node
+	Value rune
+}
+
 func main() {
-    fmt.Printf("hello, world\n")
+	result := checkBalance("}([)]")
+	if result {
+		fmt.Println("YES")
+	}else{
+		fmt.Println("NO")
+	}
+}
+
+func checkBalance(input string) (bool){
+	var stack *node 
+	pairs := map[rune]rune{
+		'}': '{',
+		')': '(',
+		']': '[',
+	}
+	if input == "" {
+		return true
+	}
+	for _, c := range input {
+		if stack == nil {
+			stack = new (node)
+			stack.Value = c
+			
+		}else if stack.Value == pairs[c] {
+			stack = stack.Prev
+		}else if c == '{' || c == '(' || c == '['{
+			newNode := new (node)
+			newNode.Prev = stack
+			newNode.Value = c
+			stack = newNode
+		}else{
+			return false
+		}
+	}
+	
+	return stack == nil
 }
