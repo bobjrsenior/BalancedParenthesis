1c9a9aaa8bdbbd37d4c9dddc3d216d8cb4e783c8
bobjrsenior <bobjrsenior@gmail.com>
2016-10-31 18:05:42 -0400

    Added more documentation

--- balancedParentheses.go
+++ balancedParentheses.go
@@ -2,13 +2,14 @@ package main
 
 import "fmt"
 
+// Stack Stuct to hold a stack of opening braces
 type node struct {
 	Prev *node
 	Value rune
 }
 
 func main() {
-	result := checkBalance("}([)]")
+	result := checkBalance("[](){}[[[]]]{(()){}}")
 	if result {
 		fmt.Println("YES")
 	}else{
@@ -17,28 +18,32 @@ func main() {
 }
 
 func checkBalance(input string) (bool){
-	var stack *node 
+	// Map closing braces to their corresponding open braces
 	pairs := map[rune]rune{
 		'}': '{',
 		')': '(',
 		']': '[',
 	}
-	if input == "" {
-		return true
-	}
+	
+	// Declare start of the stack
+	var stack *node 
+
 	for _, c := range input {
-		if stack == nil {
+		
+		if stack == nil { // If the stack is empty, make it a new node
 			stack = new (node)
 			stack.Value = c
-			
-		}else if stack.Value == pairs[c] {
+		}else if stack.Value == pairs[c] { // If the current value is a closing brace matching the top value on the stack
+			// Pop the stack
 			stack = stack.Prev
-		}else if c == '{' || c == '(' || c == '['{
+		}else if c == '{' || c == '(' || c == '['{ // If it is a valid open brace
+			// Push it to the stack
 			newNode := new (node)
 			newNode.Prev = stack
 			newNode.Value = c
 			stack = newNode
-		}else{
+		}else{ // If it isn't an open brace and doesn't match the top of the stack
+			// It's an invalid string
 			return false
 		}
 	}
